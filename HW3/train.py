5d784ccfa897382ed43cf5aeefc073f36e8beeee
Paolo Alberto <paoloalberto95@gmail.com>
2021-01-08 12:44:15 +0100

    dataset and labels reading

--- HW3/train.py
+++ HW3/train.py
@@ -4,8 +4,7 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # Do not print info and warning messages
-
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # Do not print info and warning messages
 os.environ["CUDA_VISIBLE_DEVICES"] = ""  # ignore GPU devices
 
 seed = 42
@@ -15,8 +14,8 @@ np.random.seed(seed)
 parser = argparse.ArgumentParser()
 parser.add_argument("--version",
                     type=str,
-                    required=True,
-                    default="big",  # togliere alla consegna
+                    #required=True,
+                    default="big",  # togliere il default alla consegna
                     help="Version of the model (big or little)")
 
 args = parser.parse_args()
@@ -33,6 +32,38 @@ def smalltraining():
 
 
 if args.version == "big":
-    bigTraining()
+    bigtraining()
 else:
-    smallTraining()
+    smalltraining()
+
+zip_path = tf.keras.utils.get_file(
+    origin="http://storage.googleapis.com/download.tensorflow.org/data/mini_speech_commands.zip",
+    fname="mini_speech_commands.zip",
+    extract=True,
+    cache_dir=".",
+    cache_subdir="data")
+
+data_dir = os.path.join(".", "data", "mini_speech_commands")
+
+train_files, val_files, test_files = [], [], []
+
+with open("kws_train_split.txt", "r") as train_file:
+    for filename in train_file:
+        train_files.append(filename[:-1])
+
+with open("kws_val_split.txt", "r") as val_file:
+    for filename in val_file:
+        val_files.append(filename[:-1])
+
+with open("kws_test_split.txt", "r") as test_file:
+    for filename in test_file:
+        test_files.append(filename[:-1])
+
+print(f"Train set size: {len(train_files)}")
+print(f"Val set size: {len(val_files)}")
+print(f"Test set size: {len(test_files)}")
+
+f = open("labels.txt", "r")
+LABELS = f.read().split(" ")
+f.close()
+
