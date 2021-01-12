5180f1fe2d22f99cfb33aa6f5191fa308ee666f9
Paolo Alberto <paoloalberto95@gmail.com>
2021-01-12 14:03:30 +0100

    robe

--- HW3/es1/client_0.py
+++ HW3/es1/client_0.py
@@ -6,95 +6,13 @@ from datetime import datetime
 import tensorflow as tf
 import os
 import tensorflow.lite as tflite
-
+from SignalGenerator import SignalGenerator
 from scipy.stats import entropy
 
 SAMPLING_RATE = 16000
 
-#### SIGNAL GENERATOR CLASS ######################################################################
-class SignalGenerator:
-    def __init__(self, labels, sampling_rate, frame_length, frame_step,
-                 num_mel_bins=None, lower_frequency=None, upper_frequency=None,
-                 num_coefficients=None, mfcc=False):
-        self.labels = labels
-        self.sampling_rate = sampling_rate
-        self.frame_length = frame_length
-        self.frame_step = frame_step
-        self.num_mel_bins = num_mel_bins
-        self.lower_frequency = lower_frequency
-        self.upper_frequency = upper_frequency
-        self.num_coefficients = num_coefficients
-        num_spectrogram_bins = (frame_length) // 2 + 1
-
-        if mfcc is True:
-            self.linear_to_mel_weight_matrix = tf.signal.linear_to_mel_weight_matrix(
-                self.num_mel_bins, num_spectrogram_bins, self.sampling_rate,
-                self.lower_frequency, self.upper_frequency)
-            self.preprocess = self.preprocess_with_mfcc
-        else:
-            self.preprocess = self.preprocess_with_stft
-
-    def read(self, file_path):
-        parts = tf.strings.split(file_path, os.path.sep)
-        label = parts[-2]
-        label_id = tf.argmax(label == self.labels)
-        audio_binary = tf.io.read_file(file_path)
-        audio, _ = tf.audio.decode_wav(audio_binary)
-        audio = tf.squeeze(audio, axis=1)
-
-        return audio, label_id
-
-    def pad(self, audio):
-        zero_padding = tf.zeros([self.sampling_rate] - tf.shape(audio), dtype=tf.float32)
-        audio = tf.concat([audio, zero_padding], 0)
-        audio.set_shape([self.sampling_rate])
-
-        return audio
-
-    def get_spectrogram(self, audio):
-        stft = tf.signal.stft(audio, frame_length=self.frame_length,
-                              frame_step=self.frame_step, fft_length=self.frame_length)
-        spectrogram = tf.abs(stft)
-
-        return spectrogram
-
-    def get_mfccs(self, spectrogram):
-        mel_spectrogram = tf.tensordot(spectrogram,
-                                       self.linear_to_mel_weight_matrix, 1)
-        log_mel_spectrogram = tf.math.log(mel_spectrogram + 1.e-6)
-        mfccs = tf.signal.mfccs_from_log_mel_spectrograms(log_mel_spectrogram)
-        mfccs = mfccs[..., :self.num_coefficients]
-
-        return mfccs
-
-    def preprocess_with_stft(self, file_path):
-        audio, label = self.read(file_path)
-        audio = self.pad(audio)
-        spectrogram = self.get_spectrogram(audio)
-        spectrogram = tf.expand_dims(spectrogram, -1)
-        spectrogram = tf.image.resize(spectrogram, [32, 32])
-
-        return spectrogram, label
-
-    def preprocess_with_mfcc(self, file_path):
-        audio, label = self.read(file_path)
-        audio = self.pad(audio)
-        spectrogram = self.get_spectrogram(audio)
-        mfccs = self.get_mfccs(spectrogram)
-        mfccs = tf.expand_dims(mfccs, -1)
-
-        return mfccs, label
-
-    def make_dataset(self, files, train):
-        ds = tf.data.Dataset.from_tensor_slices(files)
-        ds = ds.map(self.preprocess, num_parallel_calls=4)
-        ds = ds.batch(32)
-        ds = ds.cache()
-        if train is True:
-            ds = ds.shuffle(100, reshuffle_each_iteration=True)
-
-        return ds
-
+COMM_COST = 0
+URL = "http://192.168.1.170:8080"  # url del pc
 
 def SuccessChecker_BinEntropy(inf_array, threshold):
     if entropy(inf_array, base=2) <= threshold:
@@ -110,18 +28,11 @@ def SuccessChecker_FirstSecond(inf_array, threshold):
     else:
         return False
 
-
-#####################################################
-
-COMM_COST = 0
-URL = "http://192.168.1.170:8080"  # url del pc
-
-
-def BigRequest(url, file_path):
+def BigRequest(url, file_path, generator):
     dateTimeObj = datetime.now()
     timestamp = dateTimeObj.strftime("%d-%b-%Y (%H:%M:%S.%f)")
 
-    wav = tf.io.read_file(file_path).numpy()
+    wav, _ = generator.read(file_path)
     encoded_audio = base64.b64encode(wav).decode()
 
     json_audio = {"n": "audio", "u": "", "t": timestamp, "v": encoded_audio}
@@ -175,6 +86,7 @@ for n, (x, y_true) in enumerate(test_ds.unbatch().batch(1)):
     y_pred = interpreter.get_tensor(output_details[0]['index'])
 
     y_pred = y_pred.squeeze()  # remove batch dim
+    y_pred = tf.nn.softmax(y_pred)
 
     print("output of the little model : " + str(y_pred) + "\n")
 
@@ -184,7 +96,6 @@ for n, (x, y_true) in enumerate(test_ds.unbatch().batch(1)):
     accuracy += y_predicted_value == y_true  # 1 if True, 0 otherwise
     count += 1
 
-    y_pred = tf.nn.softmax(y_pred)
     inference = y_pred
 
     entr += entropy(inference, base=2)
@@ -192,7 +103,7 @@ for n, (x, y_true) in enumerate(test_ds.unbatch().batch(1)):
     if not SuccessChecker_BinEntropy(inference, 0.8):
         print("NO SUCCESS")
         insucces_count += 1
-        label, cost = BigRequest(URL, test_files[n])
+        label, cost = BigRequest(URL, test_files[n], generator)
         print("Prediction is : " + str(label) + "\n")
         new_accuracy += label == y_true  # 1 if True, 0 otherwise
         if label != "ERROR":
--- HW3/es1/train.py
+++ HW3/es1/train.py
@@ -6,6 +6,7 @@ from tensorflow import keras
 import zlib  # to compress the model
 import tensorflow.lite as tflite
 import tensorflow_model_optimization as tfmot  # for magnitude based pruning
+from SignalGenerator import SignalGenerator
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--version",
@@ -19,92 +20,6 @@ args = parser.parse_args()
 assert args.version in ["big", "little"], "Error: parameter version is not correct"
 
 
-#### SIGNAL GENERATOR CLASS ######################################################################
-class SignalGenerator:
-    def __init__(self, labels, sampling_rate, frame_length, frame_step,
-                 num_mel_bins=None, lower_frequency=None, upper_frequency=None,
-                 num_coefficients=None, mfcc=False):
-        self.labels = labels
-        self.sampling_rate = sampling_rate
-        self.frame_length = frame_length
-        self.frame_step = frame_step
-        self.num_mel_bins = num_mel_bins
-        self.lower_frequency = lower_frequency
-        self.upper_frequency = upper_frequency
-        self.num_coefficients = num_coefficients
-        num_spectrogram_bins = (frame_length) // 2 + 1
-
-        if mfcc is True:
-            self.linear_to_mel_weight_matrix = tf.signal.linear_to_mel_weight_matrix(
-                self.num_mel_bins, num_spectrogram_bins, self.sampling_rate,
-                self.lower_frequency, self.upper_frequency)
-            self.preprocess = self.preprocess_with_mfcc
-        else:
-            self.preprocess = self.preprocess_with_stft
-
-    def read(self, file_path):
-        parts = tf.strings.split(file_path, os.path.sep)
-        label = parts[-2]
-        label_id = tf.argmax(label == self.labels)
-        audio_binary = tf.io.read_file(file_path)
-        audio, _ = tf.audio.decode_wav(audio_binary)
-        audio = tf.squeeze(audio, axis=1)
-
-        return audio, label_id
-
-    def pad(self, audio):
-        zero_padding = tf.zeros([self.sampling_rate] - tf.shape(audio), dtype=tf.float32)
-        audio = tf.concat([audio, zero_padding], 0)
-        audio.set_shape([self.sampling_rate])
-
-        return audio
-
-    def get_spectrogram(self, audio):
-        stft = tf.signal.stft(audio, frame_length=self.frame_length,
-                              frame_step=self.frame_step, fft_length=self.frame_length)
-        spectrogram = tf.abs(stft)
-
-        return spectrogram
-
-    def get_mfccs(self, spectrogram):
-        mel_spectrogram = tf.tensordot(spectrogram,
-                                       self.linear_to_mel_weight_matrix, 1)
-        log_mel_spectrogram = tf.math.log(mel_spectrogram + 1.e-6)
-        mfccs = tf.signal.mfccs_from_log_mel_spectrograms(log_mel_spectrogram)
-        mfccs = mfccs[..., :self.num_coefficients]
-
-        return mfccs
-
-    def preprocess_with_stft(self, file_path):
-        audio, label = self.read(file_path)
-        audio = self.pad(audio)
-        spectrogram = self.get_spectrogram(audio)
-        spectrogram = tf.expand_dims(spectrogram, -1)
-        spectrogram = tf.image.resize(spectrogram, [32, 32])
-
-        return spectrogram, label
-
-    def preprocess_with_mfcc(self, file_path):
-        audio, label = self.read(file_path)
-        audio = self.pad(audio)
-        spectrogram = self.get_spectrogram(audio)
-        mfccs = self.get_mfccs(spectrogram)
-        mfccs = tf.expand_dims(mfccs, -1)
-
-        return mfccs, label
-
-    def make_dataset(self, files, train):
-        ds = tf.data.Dataset.from_tensor_slices(files)
-        ds = ds.map(self.preprocess, num_parallel_calls=4)
-        ds = ds.batch(32)
-        ds = ds.cache()
-        if train is True:
-            ds = ds.shuffle(100, reshuffle_each_iteration=True)
-
-        return ds
-
-
-##################################################################################################
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # Do not print info and warning messages
 os.environ["CUDA_VISIBLE_DEVICES"] = ""  # ignore GPU devices
