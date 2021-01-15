a67859f096c6437af09bccb1341a75324c47a79f
lorenzodenisi <denisilorenzo@gmail.com>
2021-01-15 01:29:45 +0100

    added communication limit and raised the threshold

--- HW3/es1/raspberry/client_0.py
+++ HW3/es1/raspberry/client_0.py
@@ -14,6 +14,8 @@ import time
 SAMPLING_RATE = 16000
 
 COMM_COST = 0
+MAX_COMM_COST = 4.5*(2**20)
+
 URL = "http://0.0.0.0:8080"  # url del pc
 
 
@@ -39,28 +41,30 @@ def SuccessChecker_FirstSecond(inf_array, threshold):
         return False
 
 def BigRequest(url, file_path, generator):
-    dateTimeObj = datetime.now()
-    timestamp = dateTimeObj.strftime("%d-%b-%Y (%H:%M:%S.%f)")
+	dateTimeObj = datetime.now()
+	timestamp = dateTimeObj.strftime("%d-%b-%Y (%H:%M:%S.%f)")
 	
-    wav, _ = generator.read(file_path)
+	wav, _ = generator.read(file_path)
     
-    wav = float32_to_int16(wav.numpy())
+	wav = float32_to_int16(wav.numpy())
     
-    encoded_audio = base64.b64encode(wav).decode()
-    #encoded_audio = base64.b85encode(wav).decode()
+	encoded_audio = base64.b64encode(wav).decode()
+	#encoded_audio = base64.b85encode(wav).decode()
 
-    json_audio = {"n": "audio", "u": "", "t": timestamp, "v": encoded_audio}
-    out = {"bn": "little_service", "e": json_audio}
+	json_audio = {"n": "audio", "u": "", "t": timestamp, "v": encoded_audio}
+	out = {"bn": "little_service", "e": json_audio}
     
-    
-    try:
-        r = requests.post(url=url, json=out)
-    except requests.exceptions.RequestException as err:
-        raise SystemExit(err)
+	if (COMM_COST+len(json.dumps(out)))>MAX_COMM_COST:
+		return None, 0
+	else:
+		try:
+			r = requests.post(url=url, json=out)
+		except requests.exceptions.RequestException as err:
+			raise SystemExit(err)
 
-    body = r.json()
-    sample_label = body["e"]["label"]
-    return int(sample_label), len(json.dumps(out))
+		body = r.json()
+		sample_label = body["e"]["label"]
+		return int(sample_label), len(json.dumps(out))
 
 
 #### DATASET DOWNLOAD ############################################################################
@@ -90,8 +94,6 @@ STFT_OPTIONS = {'frame_length': 256, 'frame_step': 128, 'mfcc': False}
 options = STFT_OPTIONS
 
 generator = SignalGenerator(LABELS, SAMPLING_RATE, **options)
-
-#test_ds = generator.make_dataset(test_files, False)
 
 interpreter = tflite.Interpreter('models/Group7_little.tflite')
 
@@ -139,25 +141,33 @@ for n, path in enumerate(test_files):
 
     entr += entropy(inference, base=2)
 
-    if not SuccessChecker_FirstSecond(inference, 0.59):
+    if not SuccessChecker_FirstSecond(inference, 0.58):
         print("NO SUCCESS")
         insucces_count += 1
         label, cost = BigRequest(URL, test_files[n], generator)
-        #print("Prediction is : " + str(label) + "\n")
-        new_accuracy += label == y_true  # 1 if True, 0 otherwise
-        COMM_COST += cost
-       	
-        print(f"Big model prediction: {label}, true value: {y_true}")
-        if label == y_true:
-        	big_right += 1	
+        
+        # if the function returns None, 0 it means that we are exceeding the communication limit
+        # so the big client inference is skipped and the label is taken from the little model instead
+        if label==None and cost==0:
+        	new_accuracy += y_predicted_value == y_true  # 1 if True, 0 otherwise
+        	print("Communication cost exceeded, the inference is limited to only little model now")
+        	if y_predicted_value == y_true:
+        		small_right += 1
+       		else:
+        		print(f"MISTAKE!: Predictions: {y_pred}")
+        	
+        else:
+            new_accuracy += label == y_true  # 1 if True, 0 otherwise
+            COMM_COST += cost
+            print(f"Big model prediction: {label}, true value: {y_true}")
+            if label == y_true:
+            	big_right += 1	
        	
     else:
         new_accuracy += y_predicted_value == y_true  # 1 if True, 0 otherwise
         print("SUCCESS. Prediction is " + str(y_predicted_value) + "\n")
         if y_predicted_value == y_true:
         	small_right += 1
-        else:
-        	print(f"MISTAKE!: Predictions: {y_pred}")
         
         
     print(f"recording #{n}: current accuracy: {new_accuracy/(n+1):.4f}")
@@ -166,7 +176,7 @@ accuracy /= float(count)
 
 new_accuracy /= float(count)
 
-print("Accuracy {}".format(accuracy))
+#print("Accuracy {}".format(accuracy))
 print("New accuracy {}".format(new_accuracy))
 
 print("There were : " + str(insucces_count) +  "/" + str(count)  + " errors")
@@ -175,10 +185,10 @@ print(f"Partial small accuracy: {small_right/(count-insucces_count)}")
 print(f"Partial big accuracy: {big_right/insucces_count}")
 
 print("average entropy: " + str( entr / count ))
-print(f"communication cost : {COMM_COST/(2**20):.2f} MB")
+print(f"communication cost : {COMM_COST/(2**20):.5f} MB")
 
-print(f"Average total time : {total_time*1000/count:.2f} ms")
-print(f"Average inference time : {inference_time*1000/count:.2f} ms")
+print(f"Average total time : {total_time*1000/count:.4f} ms")
+print(f"Average inference time : {inference_time*1000/count:.4f} ms")
 
 
 
